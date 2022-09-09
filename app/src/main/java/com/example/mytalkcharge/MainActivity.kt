8319807c789f7db8a82100db94b473dec562b17e
Yashendra Shukla <yashendrashukla@joshtalks.com>
2022-09-09 14:16:16 +0530

    project functionality finalizing

--- app/src/main/java/com/example/mytalkcharge/MainActivity.kt
+++ app/src/main/java/com/example/mytalkcharge/MainActivity.kt
@@ -5,13 +5,15 @@ import android.location.Address
 import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.mytalkcharge.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -40,7 +42,6 @@ class MainActivity : AppCompatActivity() {
         hourlyUpdateAdapter= HourlyWeatherListAdapter()
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
         binding.hourlyList.adapter=hourlyUpdateAdapter
-
         fusedLocationProviderClient=LocationServices.getFusedLocationProviderClient(this)
 
         checkLocationPermission()
@@ -48,15 +49,20 @@ class MainActivity : AppCompatActivity() {
         binding.swipeLayout.setOnRefreshListener {
             viewModel.getWeather(state)
         }
-
+        alphaAnimation(binding.hourlyList)
         binding.swipeLayout.setOnChildScrollUpCallback { parent, child -> binding.scrollable.getScrollY() > 10 }
 
-//        binding.hourlyCard.alpha=0.5f
-//        binding.dailyCard.alpha=0.5f
-//        binding.details.alpha=0.5f
-        binding.motionView1.transitionToStart()
         addObserver()
         trackRecordRoomState()
+    }
+
+    fun alphaAnimation(view: View) {
+        val animation = AlphaAnimation(0f, 1f)
+        animation.duration = 850
+        animation.interpolator = AccelerateDecelerateInterpolator()
+        animation.repeatCount = 1
+        animation.repeatMode = Animation.REVERSE
+        view.startAnimation(animation)
     }
 
     fun checkLocationPermission(){
@@ -70,14 +76,11 @@ class MainActivity : AppCompatActivity() {
         }
         task.addOnSuccessListener {
             val geocoder = Geocoder(this, Locale.getDefault())
-            Log.i("locationService", "checkLocationPermission: $it")
             val addresses: List<Address> = geocoder.getFromLocation(it.latitude, it.longitude, 1)
-            Log.i("locationService", "onCreate: $addresses")
             val address: String = addresses[0].getAddressLine(0)
             val city: String = addresses[0].getLocality()
              state = addresses[0].getAdminArea()
             val zip: String = addresses[0].getPostalCode()
-            Log.i("locationService", "checkLocationPermission: $city -- $state")
             viewModel.getWeather(state)
             val country: String = addresses[0].getCountryName()
 
@@ -92,15 +95,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         viewModel.currData.observe(this){
-
             binding.apply {
                 tempMax.text=String.format("%.2f", (it.main.temp_max-273))+"°C"
                 tempMin.text=String.format("%.2f", (it.main.temp_min-273))+"°C"
                 swipeLayout.isRefreshing = false
-
                 currTemp.text=String.format("%.2f", (it.main.temp-273))
             }
-
         }
 
         viewModel.responseData.observe(this){
@@ -108,7 +108,6 @@ class MainActivity : AppCompatActivity() {
                 town.text = it.city.name
                 sunRise.text=SimpleDateFormat("hh:mm a").format(it.city.sunrise*1000).toString()
                 sunSet.text=SimpleDateFormat("hh:mm a").format(it.city.sunset*1000).toString()
-                Log.i("epochTotime", "addObserver: ${it.city.sunset}")
                 weatherDesc.text=it.list[0].weather[0].main
                 windTxt.text = it.list[0].wind.speed.toString()+"km/h"
                 visibilityTxt.text=(it.list[0].visibility/1000).toString()+ "km"
@@ -118,14 +117,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun trackRecordRoomState() {
-        Log.i("motionRoomState", "trackRecordRoomState: ")
         binding.motionView1.addTransitionListener(object : MotionLayout.TransitionListener{
             override fun onTransitionStarted(
                 motionLayout: MotionLayout?,
                 startId: Int,
                 endId: Int
             ) {
-                Log.i("motionRoomState", "onTransitionStarted: $startId-- $endId")
+                //to be used for improvisation possible in future
             }
 
             override fun onTransitionChange(
@@ -134,14 +132,14 @@ class MainActivity : AppCompatActivity() {
                 endId: Int,
                 progress: Float
             ) {
-                Log.i("motionRoomState", "onTransitionChange:Started :$startId --- $endId ")
+                //to be used for improvisation possible in future
             }
 
             override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
                 if (currentId == R.id.end){
-                    Log.i("motionRoomState", "onTransitionCompleted: end")
+                    //to be used for improvisation possible in future
                 } else {
-                    Log.i("motionRoomState", "onTransitionCompleted: start ")
+                    //to be used for improvisation possible in future
                 }
             }
 
@@ -151,7 +149,7 @@ class MainActivity : AppCompatActivity() {
                 positive: Boolean,
                 progress: Float
             ) {
-                Log.i("motionRoomState", "onTransitionTrigger: ")
+                //to be used for improvisation possible in future
             }
 
         })
--- app/src/main/java/com/example/mytalkcharge/MainViewModel.kt
+++ app/src/main/java/com/example/mytalkcharge/MainViewModel.kt
@@ -1,7 +1,5 @@
 package com.example.mytalkcharge
 
-import android.util.Log
-import androidx.databinding.ObservableBoolean
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -22,39 +20,38 @@ class MainViewModel:ViewModel() {
 
 
     fun getWeather(state: String) {
-        Log.i("weatherapi", "getWeather: $state ")
         viewModelScope.launch {
             try {
-                val res=retrofitInstance.getWeatherInfo(state,"c3528ccdd115c63801af22bc093f916c")
+                val res=retrofitInstance.getWeatherInfo(state,apikey)
                 if(res.isSuccessful){
                     currData.value=res.body()
                     res.body()?.id?.let {
                         getWeather(it) }
-                    Log.i("weatherapi", "getWeather: success ${res.body()}")
                 }
-                else
-                    Log.i("weatherapi", "getWeather: failed ${res.code()} --- ${res.message()}")
+                else{
+
+                }
             }catch (ex:Exception){
-                Log.i("weatherapi", "getWeather: exception: ${ex.message}")
 
             }
         }
     }
 
     fun getWeather(id:Int) {
-        Log.i("weatherapi", "getWeather: $id ")
         viewModelScope.launch {
             try {
-                val res=retrofitInstance.getWeatherInfo(id,"c3528ccdd115c63801af22bc093f916c")
+                val res=retrofitInstance.getWeatherInfo(id,apikey)
                 if(res.isSuccessful){
-                    hourlyList.value=res.body()?.list
-                    responseData.value= res.body()!!
-                    Log.i("weatherapi", "getWeather: success ${res.body()}")
+                    res.body()?.let {
+                        hourlyList.value=it.list
+                        responseData.value= it
+                    }
+
                 }
-                else
-                    Log.i("weatherapi", "getWeather: failed ${res.code()} --- ${res.message()}")
+                else{
+
+                }
             }catch (ex:Exception){
-                Log.i("weatherapi", "getWeather: exception: ${ex.message}")
 
             }
         }
